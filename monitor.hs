2c5df5422b4d5a6732e799a63fd0cc65090cc61d
Real Limoges <b.real.limoges@gmail.com>
2025-11-26 23:14:02 -0800

    additional fleshing out of idea

--- monitor.hs
+++ monitor.hs
@@ -1,14 +1,18 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-20.5 script --package aeson --package process --package bytestring --package text --package lens --package lens-aeson
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric      #-}
 {-# LANGUAGE NumericUnderscores #-}
+{-# LANGUAGE OverloadedStrings  #-}
 
-import Control.Monad(forever)
-import Control.Lens ((^?), (^.))
-import Data.Aeson (decode)
-import qualified Data.Text as Text
+import           Control.Concurrent         (threadDelay)
+import           Control.Lens               ((^.), (^?))
+import           Control.Monad              (forever, void)
+import           Data.Aeson                 (Value, decode)
+import           Data.Aeson.Lens            (_String, key, nth)
 import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.Text                  as T
+import           System.IO.Error            (tryIOError)
+import           System.Process             (callCommand, readProcess)
 
 data ServiceStatus
     = RunningHealthy
@@ -22,7 +26,17 @@ configServiceName = "ml-service"
 
 parseStatus :: BL.ByteString -> ServiceStatus
 parseStatus json =
-    let state = ...
+    let state = json ^? nth 0 . key "State"
+        status = state >>= (^? key "Status" . _String)
+        health = state >>= (^? key "Health" . key "Status" . _String)
+
+    in case status of
+        Just "running" ->
+            case health of
+                Just "unhealthy" -> RunningUnhealthy
+                _                -> RunningHealthy
+        Just other -> Stopped (T.unpack other)
+        Nothing    -> Missing
 
 getDockerStatus :: String -> IO ServiceStatus
 getDockerStatus name = do
@@ -41,18 +55,18 @@ main = do
 
         case currentStatus of
             RunningUnhealthy -> do
-                putStrLn configServiceName ++ " is unhealthy!"
-                todo Notify
-            
+                putStrLn $ configServiceName ++ " is unhealthy!"
+                -- todo Notify
+
             Stopped reason -> do
-                putStrLn configServiceName ++ " is stopped: " ++ reason
-                todo Notify
-            
+                putStrLn $ configServiceName ++ " is stopped: " ++ reason
+                -- todo Notify
+
             Missing -> do
-                putStrLn configServiceName ++ " is not found."
-                notify?
+                putStrLn $ configServiceName ++ " is not found."
+                -- notify?
 
             RunningHealthy ->
-                putStrLn configServiceName ++ " is working as expected."
-    
+                putStrLn $ configServiceName ++ " is working as expected."
+
         threadDelay 5_000_000  -- this is in microseconds
