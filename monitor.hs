a3aa2cb3ef0323368d1cfe0384317ac94fefacb4
Real Limoges <b.real.limoges@gmail.com>
2025-11-27 09:48:32 -0800

    fixed osascript; handling of Docker Status

--- monitor.hs
+++ monitor.hs
@@ -1,6 +1,6 @@
 #!/usr/bin/env stack
--- stack --resolver lts-20.5 script --package aeson --package process --package bytestring --package text --package lens --package lens-aeson
-{-# LANGUAGE DeriveGeneric      #-}
+-- stack --resolver lts-22.11 script --package aeson --package process --package bytestring --package text --package lens --package lens-aeson
+
 {-# LANGUAGE NumericUnderscores #-}
 {-# LANGUAGE OverloadedStrings  #-}
 
@@ -11,8 +11,11 @@ import           Data.Aeson                 (Value, decode)
 import           Data.Aeson.Lens            (_String, key, nth)
 import qualified Data.ByteString.Lazy.Char8 as BL
 import qualified Data.Text                  as T
+import           System.Exit                (ExitCode (..))
+import           System.Info                (os)
 import           System.IO.Error            (tryIOError)
-import           System.Process             (callCommand, readProcess)
+import           System.Process             (callCommand, readProcess,
+                                             readProcessWithExitCode)
 
 data ServiceStatus
     = RunningHealthy
@@ -26,7 +29,7 @@ configServiceName = "ml-service"
 
 parseStatus :: BL.ByteString -> ServiceStatus
 parseStatus json =
-    let state = json ^? nth 0 . key "State"
+    let state  = json ^? nth 0 . key "State"
         status = state >>= (^? key "Status" . _String)
         health = state >>= (^? key "Health" . key "Status" . _String)
 
@@ -40,10 +43,19 @@ parseStatus json =
 
 getDockerStatus :: String -> IO ServiceStatus
 getDockerStatus name = do
-    result <- tryIOError $ readProcess "docker" ["inspect", name] ""
-    return $ case result of
-        Right output -> parseStatus (BL.pack output)
-        Left _       -> Missing
+    (exitCode, output, _err) <- readProcessWithExitCode "docker" ["inspect", name] ""
+    return $ case exitCode of
+        ExitSuccess   -> parseStatus (BL.pack output)
+        ExitFailure _ -> Missing
+
+
+notify :: String -> String -> IO ()
+notify title msg = do
+    _ <- tryIOError $ callCommand $ case System.Info.os of
+        "darwin" -> "osascript -e 'display notification \"" ++ msg ++ "\" with title \"" ++ title ++ "\"'"
+        "linux"  -> "notify-send \"" ++ title ++ "\" \"" ++ msg ++ "\""
+        _        -> "echo 'Alert: " ++ msg ++ "'"
+    return ()
 
 
 main :: IO ()
@@ -56,15 +68,15 @@ main = do
         case currentStatus of
             RunningUnhealthy -> do
                 putStrLn $ configServiceName ++ " is unhealthy!"
-                -- todo Notify
+                notify "ML Service Alert" "Service is Running but Unhealthy"
 
             Stopped reason -> do
                 putStrLn $ configServiceName ++ " is stopped: " ++ reason
-                -- todo Notify
+                notify "ML Service Alert" $ "Service Stopped: " ++ reason
 
             Missing -> do
-                putStrLn $ configServiceName ++ " is not found."
-                -- notify?
+                 putStrLn $ configServiceName ++ " is not found."
+                 notify "ML Service Alert" $ "Service Is Not Found"
 
             RunningHealthy ->
                 putStrLn $ configServiceName ++ " is working as expected."
